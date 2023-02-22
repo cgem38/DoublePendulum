57d3cff4627694298d7cee868db8ef49ec5879fc
Chris Myers <cgmyers38@gmail.com>
2023-02-22 12:14:03 -0700

    Added initial input values and connected all sliders/input fields

--- doublePendulumSolver.py
+++ doublePendulumSolver.py
@@ -5,81 +5,90 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import PillowWriter
-import time
 
-startTime = time.time()
-t, g = smp.symbols('t g')
-m1, m2 = smp.symbols('m1 m2')
-L1, L2 = smp.symbols('L1, L2')
 
-#Since theta1 and theta2 are strictly functions of time, here they are defined symbolically as 
-#such
-theta1, theta2 = smp.symbols(r'\theta1, \theta2', cls=smp.Function)
-theta1 = theta1(t)
-theta2 = theta2(t)
+def Solver(gravity, time, mass1, mass2, length1, length2, theta10, theta20):
+    t, g = smp.symbols('t g')
+    m1, m2 = smp.symbols('m1 m2')
+    L1, L2 = smp.symbols('L1, L2')
 
-#This defines the first and second derivatives of theta1 and theta2
-theta1_d = smp.diff(theta1, t)
-theta2_d = smp.diff(theta2, t)
-theta1_2d = smp.diff(theta1_d, t)
-theta2_2d = smp.diff(theta2_d, t)
+    #Since theta1 and theta2 are strictly functions of time, here they are defined symbolically as 
+    #such
+    theta1, theta2 = smp.symbols(r'\theta1, \theta2', cls=smp.Function)
+    theta1 = theta1(t)
+    theta2 = theta2(t)
 
-#Cartesian coordinates of m1 and m2
-x1 = L1 * smp.sin(theta1)
-y1 = -L1 * smp.cos(theta1)
-x2 = x1 + (L2 * smp.sin(theta2))
-y2 = y1 + (-L2 * smp.cos(theta2))
+    #This defines the first and second derivatives of theta1 and theta2
+    theta1_d = smp.diff(theta1, t)
+    theta2_d = smp.diff(theta2, t)
+    theta1_2d = smp.diff(theta1_d, t)
+    theta2_2d = smp.diff(theta2_d, t)
 
-#Kinetic energy
-T1 = 1/2 * m1 * (smp.diff(x1, t)**2 + smp.diff(y1, t)**2)
-T2 = 1/2 * m2 * (smp.diff(x2, t)**2 + smp.diff(y2, t)**2)
-T = T1 + T2
+    #Cartesian coordinates of m1 and m2
+    x1 = L1 * smp.sin(theta1)
+    y1 = -L1 * smp.cos(theta1)
+    x2 = x1 + (L2 * smp.sin(theta2))
+    y2 = y1 + (-L2 * smp.cos(theta2))
 
-#Potential Energy
-V1 = m1 * g * y1
-V2 = m2 * g * y2
-V = V1 + V2
+    #Kinetic energy
+    T1 = 1/2 * m1 * (smp.diff(x1, t)**2 + smp.diff(y1, t)**2)
+    T2 = 1/2 * m2 * (smp.diff(x2, t)**2 + smp.diff(y2, t)**2)
+    T = T1 + T2
 
-#LaGrangian
-L = T - V
+    #Potential Energy
+    V1 = m1 * g * y1
+    V2 = m2 * g * y2
+    V = V1 + V2
 
-#Define LaGrange's Equations
-LE1 = smp.diff(L, theta1) - smp.diff(smp.diff(L, theta1_d), t).simplify()
-LE2 = smp.diff(L, theta2) - smp.diff(smp.diff(L, theta2_d), t).simplify()
+    #LaGrangian
+    L = T - V
 
-solutions = smp.solve([LE1, LE2], (theta1_2d, theta2_2d), simplify=False, rational=False)
+    #Define LaGrange's Equations
+    LE1 = smp.diff(L, theta1) - smp.diff(smp.diff(L, theta1_d), t).simplify()
+    LE2 = smp.diff(L, theta2) - smp.diff(smp.diff(L, theta2_d), t).simplify()
 
-#Converts the symbolic functions into numerical functions that can be solved to return numeric answers 
-dz1dt_f = smp.lambdify((t,g,m1,m2,L1,L2,theta1,theta2,theta1_d,theta2_d), solutions[theta1_2d])
-dz2dt_f = smp.lambdify((t,g,m1,m2,L1,L2,theta1,theta2,theta1_d,theta2_d), solutions[theta2_2d])
-dtheta1dt_f = smp.lambdify(theta1_d, theta1_d)
-dtheta2dt_f = smp.lambdify(theta2_d, theta2_d)
+    solutions = smp.solve([LE1, LE2], (theta1_2d, theta2_2d), simplify=False, rational=False)
 
-def dSdt(S, t, g, m1, m2, L1, L2):
-    theta1, z1, theta2, z2 = S
-    return [dtheta1dt_f(z1),
-            dz1dt_f(t, g, m1, m2, L1, L2, theta1, theta2, z1, z2),
-            dtheta2dt_f(z2), 
-            dz2dt_f(t, g, m1, m2, L1, L2, theta1, theta2, z1, z2)]
+    #Converts the symbolic functions into numerical functions that can be solved to return numeric answers 
+    dz1dt_f = smp.lambdify((t,g,m1,m2,L1,L2,theta1,theta2,theta1_d,theta2_d), solutions[theta1_2d])
+    dz2dt_f = smp.lambdify((t,g,m1,m2,L1,L2,theta1,theta2,theta1_d,theta2_d), solutions[theta2_2d])
+    dtheta1dt_f = smp.lambdify(theta1_d, theta1_d)
+    dtheta2dt_f = smp.lambdify(theta2_d, theta2_d)
 
-t = np.linspace(0, 40, 1001)
-g = 9.81
-m1 = 2
-m2 = 1
-L1 = 2
-L2 = 1
+    def dSdt(S, t, g, m1, m2, L1, L2):
+        theta1, z1, theta2, z2 = S
+        return [dtheta1dt_f(z1),
+                dz1dt_f(t, g, m1, m2, L1, L2, theta1, theta2, z1, z2),
+                dtheta2dt_f(z2), 
+                dz2dt_f(t, g, m1, m2, L1, L2, theta1, theta2, z1, z2)]
 
-#IMPORTANT Regarding y0: y0 represents the intitial conditions. Here, the terms in the order
-#called in ans are: [theta1, d(theta1)/d(t)=angular velocity of theta1, theta2, d(theta2)/d(t)]
-#Also of note: all of these units are in radians/ radians/sec.
-#Finally, args is necessary here because these arguments need to be passed to the solver. Obviously. 
-ans = odeint(dSdt, y0=[1, -3, -1, 5], t=t, args=(g, m1, m2, L1, L2))
-endTime = time.time()
-theta1 = ans.T[0]
-z1 = ans.T[1]
-theta2 = ans.T[2]
-z2 = ans.T[3]
-plot = plt.plot(t, theta2)
-plt.show()
-executionTime = endTime - startTime
-print("execution time: ", executionTime)
+    t = np.linspace(0, time, 1001)
+    g = gravity
+    m1 = mass1
+    m2 = mass2
+    L1 = length1
+    L2 = length2
+
+    #IMPORTANT Regarding y0: y0 represents the intitial conditions. Here, the terms in the order
+    #called in ans are: [theta1, d(theta1)/d(t)=angular velocity of theta1, theta2, d(theta2)/d(t)]
+    #Also of note: all of these units are in radians/ radians/sec.
+    #Finally, args is necessary here because these arguments need to be passed to the solver. Obviously. 
+    ans = odeint(dSdt, y0=[theta10, 0, theta20, 0], t=t, args=(g, m1, m2, L1, L2))
+    solTheta1 = ans.T[0]
+    solZ1 = ans.T[1]
+    solTheta2 = ans.T[2]
+    solZ2 = ans.T[3]
+    solx1 = L1 * np.sin(solTheta1)
+    soly1 = -L1 * np.cos(solTheta1)
+    solx2 = solx1 + L2 * np.sin(solTheta2)
+    soly2 = soly1 + (-L2 * np.cos(solTheta2))
+    # plt.plot(solx1, soly1, label="mass1")
+    # plt.plot(solx2, soly2, label="mass2")
+    # plt.legend()
+    # plt.show()
+
+    return ans
+
+if __name__ == "__main__":
+    Solver(9.81, 10, 1, 1, 1, 1, 1)
+    
--- mainWindow.py
+++ mainWindow.py
@@ -10,101 +10,189 @@ from scipy.integrate import odeint, solve_ivp
 import scipy as sp
 import matplotlib.pyplot as plt
 from slider import Slider
-import time
+from doublePendulumSolver import Solver
 import sympy as smp
 
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.positionGraph = self.createPositionGraph()
-        variableComboBox = self.createVariableComboBox()
+        self.positionGraph = self.createGraphWidget()
+        variableComboBox = self.createVariableGroupBox()
+        initialConditionsComboBox = self.createInitialConditionsGroupBox()
 
 
         runButton = QPushButton("Run")
         runButton.pressed.connect(self.runButtonPressed)
-        #BEGINNING OF CODE TO TEST GRAPHING FUNCTION -- DELETE WHEN ODE SOLVER IS COMPLETED
-        self.x = np.linspace(0, 10, 1000)
-        self.y = self.x**2
+        
+        #Temporary Variables to test ODE solver and grpahing functions, since the UI does not have input for gravity and time yet. Delete when it does. 
+        self.g = 9.81
+        self.t = 10
         #END OF CODE TO TEST GRAPHING FUNCTION
 
         tab1MainLayout = QGridLayout()
         tab1MainLayout.addWidget(self.positionGraph, 0, 0, 1, 6)
         tab1MainLayout.addWidget(variableComboBox, 1, 0, 1, 6)
-        tab1MainLayout.addWidget(runButton, 2, 2, 1, 2)
+        tab1MainLayout.addWidget(initialConditionsComboBox, 2, 0, 1, 6)
+        tab1MainLayout.addWidget(runButton, 3, 2, 1, 2)
         tab1MainLayout.setRowStretch(0, 5)
         tab1MainLayout.setRowStretch(1, 1)
         tab1MainLayout.setRowStretch(2, 1)
+        tab1MainLayout.setRowStretch(3, 1)
         self.setLayout(tab1MainLayout)
-
-        
     
     def runButtonPressed(self):
-        self.graphPositions()
+        self.inputValuesChanged()
+        g = self.g
+        t = self.t
+        m1 = float(self.mass1Input.text())
+        m2 = float(self.mass2Input.text())
+        L1 = float(self.rod1LengthInput.text())
+        L2 = float(self.rod2LengthInput.text())
+        theta10 = float(self.theta1InitialInput.text())
+        theta20 = float(self.theta2InitialInput.text())
+        output = Solver(g, t, m1, m2, L1, L2, theta10, theta20)
+        self.graphPositions(output)
+        return output
     
-    def graphPositions(self):
-        # startTime = time.time()
-        # timeInterval = 1/60
-        # for i in range(int(len(self.x)/2)):
-        #     currentSpot = 2 * i
-        #     xPoints = [self.x[currentSpot], self.x[currentSpot + 1]]
-        #     yPoints = [self.y[currentSpot], self.y[currentSpot + 1]]
-        #     self.positionGraph.plot(xPoints, yPoints)
-        #     time.sleep(timeInterval) 
+    def graphPositions(self, solverOutput):
+        Theta1 = solverOutput.T[0]
+        Z1 = solverOutput.T[1]
+        Theta2 = solverOutput.T[2]
+        Z2 = solverOutput.T[3]
+        t = np.linspace(0, self.t, 1001)
+        m1 = float(self.mass1Input.text())
+        m2 = float(self.mass2Input.text())
+        L1 = float(self.rod1LengthInput.text())
+        L2 = float(self.rod2LengthInput.text())
+
+        x1 = L1 * np.sin(Theta1)
+        y1 = -L1 * np.cos(Theta1)
+        x2 = x1 + L2 * np.sin(Theta2)
+        y2 = y1 + (-L2 * np.cos(Theta2))
+
+        self.XYLine1.setData(x1, y1)
+        self.XYLine2.setData(x2, y2)
+
         pass
 
-
-
-    def createPositionGraph(self):
-        graph = pg.plot()
-        graph.showGrid(x=True, y=True)
-        graph.setLabel('left', 'Vertical Position')
-        graph.setLabel('bottom', 'Horizontal Position')
-
-        return graph
+    def createGraphWidget(self, output=[]):
+        x = []
+        y = []
+        XYgraph = pg.plot()
+        XYgraph.showGrid(x=True, y=True)
+        XYgraph.setLabel('left', 'Vertical Position')
+        XYgraph.setLabel('bottom', 'Horizontal Position')
+        XYgraph.addLegend(offset=(-1, 1))
+        self.XYLine1 = XYgraph.plot(x, y, pen='r', name="Mass 1")
+        self.XYLine2 = XYgraph.plot(x, y, pen='b', name="Mass 2")
+        
+        return XYgraph
     
-    def createVariableComboBox(self):
+    def createVariableGroupBox(self):
         Box = QGroupBox(title="Variables")
 
         rod1LengthLabel = QLabel("Rod 1 Length")
-        rod1LengthSlider = Slider(0.01, 5.0, "horizontal")
-        rod1LengthInput = QLineEdit()
+        self.rod1LengthSlider = Slider(0.01, 5.0, "horizontal")
+        self.rod1LengthInput = QLineEdit()
+        self.rod1LengthInput.setText("1")
+        self.rod1LengthSlider.setValue(int(100 * float(self.rod1LengthInput.text())))
+        self.rod1LengthSlider.valueChanged.connect(self.sliderValuesChanged)
 
         rod2LengthLabel = QLabel("Rod 2 Length")
-        rod2LengthSlider = Slider(0.01, 5.0, "horizontal")
-        rod2LengthInput = QLineEdit()
+        self.rod2LengthSlider = Slider(0.01, 5.0, "horizontal")
+        self.rod2LengthInput = QLineEdit()
+        self.rod2LengthInput.setText("1")
+        self.rod2LengthSlider.setValue(int(100 * float(self.rod2LengthInput.text())))
+        self.rod2LengthSlider.valueChanged.connect(self.sliderValuesChanged)
 
-        mass1LengthLabel = QLabel("Mass 1 Length")
-        mass1LengthSlider = Slider(0.01, 2.0, "horizontal")
-        mass1LengthInput = QLineEdit()
+        mass1Label = QLabel("Mass 1")
+        self.mass1Slider = Slider(0.01, 2.0, "horizontal")
+        self.mass1Input = QLineEdit()
+        self.mass1Input.setText("1")
+        self.mass1Slider.setValue(int(100 * float(self.mass1Input.text())))
+        self.mass1Slider.valueChanged.connect(self.sliderValuesChanged)
 
-        mass2LengthLabel = QLabel("Mass 2 Length")
-        mass2LengthSlider = Slider(0.01, 2.0, "horizontal")
-        mass2LengthInput = QLineEdit()
+
+        mass2Label = QLabel("Mass 2")
+        self.mass2Slider = Slider(0.01, 2.0, "horizontal")
+        self.mass2Input = QLineEdit()
+        self.mass2Input.setText("1")
+        self.mass2Slider.setValue(int(100 * float(self.mass2Input.text())))
+        self.mass2Slider.valueChanged.connect(self.sliderValuesChanged)
 
         layout = QGridLayout()
         layout.addWidget(rod1LengthLabel, 0, 0)
-        layout.addWidget(rod1LengthSlider, 0, 1)
-        layout.addWidget(rod1LengthInput, 0, 2)
+        layout.addWidget(self.rod1LengthSlider, 0, 1)
+        layout.addWidget(self.rod1LengthInput, 0, 2)
 
         layout.addWidget(rod2LengthLabel, 1, 0)
-        layout.addWidget(rod2LengthSlider, 1, 1)
-        layout.addWidget(rod2LengthInput, 1, 2)
+        layout.addWidget(self.rod2LengthSlider, 1, 1)
+        layout.addWidget(self.rod2LengthInput, 1, 2)
 
-        layout.addWidget(mass1LengthLabel, 0, 3)
-        layout.addWidget(mass1LengthSlider, 0, 4)
-        layout.addWidget(mass1LengthInput, 0, 5)
+        layout.addWidget(mass1Label, 0, 3)
+        layout.addWidget(self.mass1Slider, 0, 4)
+        layout.addWidget(self.mass1Input, 0, 5)
 
-        layout.addWidget(mass2LengthLabel, 1, 3)
-        layout.addWidget(mass2LengthSlider, 1, 4)
-        layout.addWidget(mass2LengthInput, 1, 5)
+        layout.addWidget(mass2Label, 1, 3)
+        layout.addWidget(self.mass2Slider, 1, 4)
+        layout.addWidget(self.mass2Input, 1, 5)
 
+        Box.setLayout(layout)
+        
+        return Box
+    
+    def createInitialConditionsGroupBox(self):
+        Box = QGroupBox(title="Initial Conditions")        
+
+        theta1InitialLabel = QLabel("Initial Theta 1 (radians)")
+        self.theta1InitialSlider = Slider(0.0, 2*np.pi, "horizontal")
+        self.theta1InitialInput = QLineEdit()
+        self.theta1InitialInput.setText("0")
+        self.theta1InitialSlider.setValue(int(100 * float(self.theta1InitialInput.text())))
+        self.theta1InitialSlider.valueChanged.connect(self.sliderValuesChanged)
+
+        theta2InitialLabel = QLabel("Initial Theta 2 (radians)")
+        self.theta2InitialSlider = Slider(0.0, 2*np.pi, "horizontal")
+        self.theta2InitialInput = QLineEdit()
+        self.theta2InitialInput.setText("0")
+        self.theta2InitialSlider.setValue(int(100 * float(self.theta2InitialInput.text())))
+        self.theta2InitialSlider.valueChanged.connect(self.sliderValuesChanged)
+
+        layout = QHBoxLayout()
+        layout.addWidget(theta1InitialLabel, 4)
+        layout.addWidget(self.theta1InitialSlider, 4)
+        layout.addWidget(self.theta1InitialInput, 1)
+        layout.addStretch(2)
+        layout.addWidget(theta2InitialLabel, 4)
+        layout.addWidget(self.theta2InitialSlider, 4)
+        layout.addWidget(self.theta2InitialInput, 1)
         Box.setLayout(layout)
 
         return Box
     
+    def sliderValuesChanged(self):
+        #System Variable values:
+        self.rod1LengthInput.setText(str(self.rod1LengthSlider.value() / 100))
+        self.rod2LengthInput.setText(str(self.rod2LengthSlider.value() / 100))
+        self.mass1Input.setText(str(self.mass1Slider.value() / 100))
+        self.mass2Input.setText(str(self.mass2Slider.value() / 100))
 
-    
+        #Initial Condition values:
+        self.theta1InitialInput.setText(str(self.theta1InitialSlider.value() / 100))
+        self.theta2InitialInput.setText(str(self.theta2InitialSlider.value() / 100))
+
+
+    def inputValuesChanged(self):
+        #System Variable values:
+        self.rod1LengthSlider.setValue(int(100 * float(self.rod1LengthInput.text())))
+        self.rod2LengthSlider.setValue(int(100 * float(self.rod2LengthInput.text())))
+        self.mass1Slider.setValue(int(100 * float(self.mass1Input.text())))
+        self.mass2Slider.setValue(int(100 * float(self.mass2Input.text())))
+
+        #Initial Condition values:
+        self.theta1InitialSlider.setValue(int(100 * float(self.theta1InitialInput.text())))
+        self.theta2InitialSlider.setValue(int(100 * float(self.theta2InitialInput.text())))
 
 app = QApplication(sys.argv)
 window = MainWindow()
